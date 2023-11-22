e6ffdfc7ce977209ad57d6ee4523f988402951f1
Kalai Selvan <kselvan@ntst.com>
2023-11-22 15:35:40 +0530

    Initial Commit

---
+++ ievolv/src/main/java/com/i2i/ievolv/IevolvApplication.java
@@ -0,0 +1,13 @@
+package com.i2i.ievolv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IevolvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IevolvApplication.class, args);
+	}
+
+}
