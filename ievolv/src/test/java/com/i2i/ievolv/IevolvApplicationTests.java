015bf9c40a2d70181699e6b26532e6770a5b6995
Kalai Selvan <95227826+rkskalai@users.noreply.github.com>
2023-11-22 15:09:51 +0530

    Initial commit

---
+++ ievolv/src/test/java/com/i2i/ievolv/IevolvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i2i.ievolv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IevolvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
